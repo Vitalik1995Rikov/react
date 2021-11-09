98f4ea5927e65eeaed655c40688f13803a306fd3
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-09 18:49:37 +0300

    project7

--- my-app/src/components/randomChar/RandomChar.js
+++ my-app/src/components/randomChar/RandomChar.js
@@ -1,29 +1,31 @@
 import {Component} from 'react';
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
     state = {
         name: null,
         description: null,
-        thumbnail: null
+        thumbnail: null,
+        homepage: null,
+        wiki: null
     }
     render() {
+        const {name, description, thumbnail, homepage, wiki} = this.state;
         return (
             <div className="randomchar">
                 <div className="randomchar__block">
-                    <img src={thor} alt="Random character" className="randomchar__img"/>
+                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
                     <div className="randomchar__info">
-                        <p className="randomchar__name">Thor</p>
+                        <p className="randomchar__name">{name}</p>
                         <p className="randomchar__descr">
-                            As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
+                           {description}
                         </p>
                         <div className="randomchar__btns">
-                            <a href="#" className="button button__main">
+                            <a href={homepage} className="button button__main">
                                 <div className="inner">homepage</div>
                             </a>
-                            <a href="#" className="button button__secondary">
+                            <a href={wiki} className="button button__secondary">
                                 <div className="inner">Wiki</div>
                             </a>
                         </div>
